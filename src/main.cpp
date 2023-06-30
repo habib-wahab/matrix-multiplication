6080b1547247de242f3feb547b336c0e2c1270f4
habib-wahab <gh@habibwahab.co.uk>
2023-06-30 09:09:30 +0100

    update comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main() 
 {
+    // test 100 x 100, 200 x 200, 300 x 300... square matrices being multiplied
     std::vector<size_t> square_matrices = { 100, 200, 300, 500, 1000, 1500, 2000 };
     IntegerMatrixTests tests(square_matrices);
     tests.RunTests();
